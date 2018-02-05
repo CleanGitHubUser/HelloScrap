8d1fda3ebb12e4552ad6a4c7414b06cf4fd81609
FatalWhite <34561520+fatalwhite@users.noreply.github.com>
2018-02-05 16:16:30 +0900

    2018-02-05

---
+++ daum_jtbc.py
@@ -0,0 +1,74 @@
+#-*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+# 다음 JTBC 뉴스 스크래핑 예제
+# media.daum.net/cp/310
+# ?page=2&regDate=20170501&cateId=1002
+
+press = [310]       # 언론사
+date = [20180205]   # 년월일
+page = [1, 2, 3]            # 페이지
+
+new_title = []      # 뉴스 제목
+new_desc = []       # 뉴스 간략 소개
+
+# 스크래핑할 URL 지정
+URL = 'http://media.daum.net/cp/' + str(press[0]) + \
+    '?page=' + str(page[0]) + '&regDate=' + str(date[0])
+
+# 스크래핑해서 소스를 source_code 에 저장
+source_code = requests.get( URL )
+
+# 중간 결과 출력
+# print( source_code.text )
+
+# 텍스트 추출을 위해 lxml로 태그 분석 (메모리 적재)
+plain_text = source_code.text
+soup = BeautifulSoup(plain_text, 'lxml')
+
+# 기사 제목 추출
+cnt = 1
+title_list = []
+for title in soup.select("a['class=link_txt']"):
+    if cnt > 15: break
+    # print(title.text)
+    # print(title.text.strip())
+    title_list.append(title)
+    cnt += 1
+
+# 기사 간단 소개 추출
+cnt = 1
+summary_list = []
+for summary in soup.select("span['class=link_txt']"):
+    if cnt > 15: break
+    # print(title.text)
+    # print(title.text.strip())
+    summary_list.append(summary)
+    cnt += 1
+
+article_list = []
+for i in range(0, len(title_list)):
+    link = soup.select("a['class=link_txt']")
+    sc = requests.get(link[i].get("href"))
+    pt = sc.text
+    sp = BeautifulSoup(pt, 'lxml')
+    article = sp.select("div['id=harmonyContainer']")
+    article_list.append(article[0])
+
+# cnt = 1
+# summary_list = []
+# for summary in soup.select("a['href=link_txt']"):
+#     if cnt > 15: break
+#     # print(title.text)
+#     # print(title.text.strip())
+#     summary_list.append(summary)
+#     cnt += 1
+
+for i in range(0, len(title_list)):
+    print(title_list[i].text.strip() + '\n')
+    # print(summary_list[i].text.strip())
+    print(article_list[i].text.strip())
+    print('\n')
+
---
+++ daum_movie.py
@@ -0,0 +1,33 @@
+#-*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+# URL = []
+# URL.append("")
+
+URL = 'http://ticket2.movie.daum.net/Movie/MovieRankList.aspx'
+source_code = requests.get(URL)
+plain_text = source_code.text
+soup = BeautifulSoup(plain_text, 'lxml')
+
+movie_list = []
+for rank in soup.select("img['class = thumb_photo']"):
+    movie_list.append(rank.get("alt").strip())
+
+rate_list = []
+for rank in soup.select("em['class=emph_grade']"):
+    rate = rank.text.strip()
+    if rate == "":
+        rate = 10
+    rate_list.append(rate)
+
+info_list = []
+for rank in soup.select("dl['class=list_state']"):
+    info_list.append(rank.text.strip())
+
+for i in range(0, len(movie_list)):
+    print(movie_list[i])
+    print('평점 : ' + str(rate_list[i]) + ' / 10')
+    print(info_list[i] + '\n')
+
